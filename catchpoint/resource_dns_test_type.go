896ecc1b500db4688d80adcea4d13c77e0ed21e0
vikashkuma <raja.vicky007.com>
2024-03-02 19:14:03 +0530

    contact group support for all test type

--- catchpoint/resource_dns_test_type.go
+++ catchpoint/resource_dns_test_type.go
@@ -366,6 +366,14 @@ func resourceDnsTestType() *schema.Resource {
 														Type: schema.TypeString,
 													},
 												},
+												"contact_groups": {
+													Type:        schema.TypeList,
+													Optional:    true,
+													Description: "List of contact groups to receive alert notifications.",
+													Elem: &schema.Schema{
+														Type: schema.TypeString,
+													},
+												},
 											},
 										},
 									},
@@ -396,6 +404,14 @@ func resourceDnsTestType() *schema.Resource {
 										Type:        schema.TypeList,
 										Optional:    true,
 										Description: "Optional. List of emails to alert",
+										Elem: &schema.Schema{
+											Type: schema.TypeString,
+										},
+									},
+									"contact_groups": {
+										Type:        schema.TypeList,
+										Optional:    true,
+										Description: "List of contact groups to receive alert notifications.",
 										Elem: &schema.Schema{
 											Type: schema.TypeString,
 										},
