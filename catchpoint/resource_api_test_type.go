5b5d10e7259649bd1a4f4fb94852347030ef4c7c
vikashkuma <raja.vicky007.com>
2024-03-05 16:32:22 +0530

    playwright and puppeteer test supported

--- catchpoint/resource_api_test_type.go
+++ catchpoint/resource_api_test_type.go
@@ -686,7 +686,7 @@ func resourceApiTestType() *schema.Resource {
 										Type:        schema.TypeSet,
 										Optional:    true,
 										MaxItems:    1,
-										Description: "Notification group for configuring alert notifications, including recipients' email addresses and alert settings.",
+										Description: "Notification group for configuring alert notifications, including recipients' email addresses and alert settings. To ensure either recipient_email_ids or contact_groups is provided",
 										Elem: &schema.Resource{
 											Schema: map[string]*schema.Schema{
 												"notify_on_warning": {
@@ -714,8 +714,8 @@ func resourceApiTestType() *schema.Resource {
 												},
 												"recipient_email_ids": {
 													Type:        schema.TypeList,
-													Required:    true,
-													Description: "List of email addresses to receive alert notifications.",
+													Optional:    true,
+													Description: "List of email addresses to receive alert notifications. To ensure either recipient_email_ids or contact_groups is provided",
 													Elem: &schema.Schema{
 														Type: schema.TypeString,
 													},
@@ -723,7 +723,7 @@ func resourceApiTestType() *schema.Resource {
 												"contact_groups": {
 													Type:        schema.TypeList,
 													Optional:    true,
-													Description: "List of contact groups to receive alert notifications.",
+													Description: "List of contact groups to receive alert notifications. To ensure either recipient_email_ids or contact_groups is provided",
 													Elem: &schema.Schema{
 														Type: schema.TypeString,
 													},
@@ -738,7 +738,7 @@ func resourceApiTestType() *schema.Resource {
 							Type:        schema.TypeSet,
 							Required:    true,
 							MaxItems:    1,
-							Description: "Notification group for setting up alert recipients, adding alert webhook ids",
+							Description: "Notification group for setting up alert recipients, adding alert webhook ids. To ensure either recipient_email_ids or contact_groups is provided",
 							Elem: &schema.Resource{
 								Schema: map[string]*schema.Schema{
 									"subject": {
@@ -757,7 +757,7 @@ func resourceApiTestType() *schema.Resource {
 									"recipient_email_ids": {
 										Type:        schema.TypeList,
 										Optional:    true,
-										Description: "Optional. List of emails to alert",
+										Description: "Optional. List of emails to alert. To ensure either recipient_email_ids or contact_groups is provided",
 										Elem: &schema.Schema{
 											Type: schema.TypeString,
 										},
@@ -765,7 +765,7 @@ func resourceApiTestType() *schema.Resource {
 									"contact_groups": {
 										Type:        schema.TypeList,
 										Optional:    true,
-										Description: "List of contact groups to receive alert notifications.",
+										Description: "List of contact groups to receive alert notifications. To ensure either recipient_email_ids or contact_groups is provided",
 										Elem: &schema.Schema{
 											Type: schema.TypeString,
 										},
