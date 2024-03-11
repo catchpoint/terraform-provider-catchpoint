5606e5a2edb9d05e1ea160b9cc5b72af5523064a
vikashkuma <raja.vicky007.com>
2024-03-11 16:22:53 +0530

    requset setting updated

--- catchpoint/flatten_all_test_type.go
+++ catchpoint/flatten_all_test_type.go
@@ -56,7 +56,7 @@ func flattenAuthenticationStruct(authentication *AuthenticationStruct) []interfa
 	if authentication != nil {
 		authMap := map[string]interface{}{
 			"authentication_type": strings.ToLower(authentication.AuthenticationMethodType.Name),
-			"username":            authentication.Username,
+			"username":            authentication.UserName,
 			"password":            authentication.Password,
 			"password_ids":        authentication.PasswordIds,
 		}
