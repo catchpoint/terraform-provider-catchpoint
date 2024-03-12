93f1a3872dcd0dfe934ca7b9c692e1b2b7788634
vikashkuma <raja.vicky007.com>
2024-03-12 10:09:16 +0530

    labels added

--- docs/examples/Create test/puppeteer_test.tf
+++ docs/examples/Create test/puppeteer_test.tf
@@ -16,6 +16,19 @@ resource "puppeteer_test" "test" {
         additional_monitor="ping icmp"
     }
 
+    request_settings {
+      authentication {
+        authentication_type = "basic"
+        password_ids = [2332]
+      }
+      token_ids = [1096]
+      http_request_headers {
+        user_agent {
+          value = "vikash"
+        }
+      }
+    }
+
     alert_settings {
         alert_rule {
             alert_type="timing"
