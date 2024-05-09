6bef7ae0861a2186cc8c3580d4d1226907074a27
vikashkuma <raja.vicky007.com>
2024-05-09 15:11:34 +0530

    notificatio group for alert group items

--- catchpoint/resource_web_test_type.go
+++ catchpoint/resource_web_test_type.go
@@ -681,8 +681,8 @@ func resourceWebTestType() *schema.Resource {
 									"notification_group": {
 										Type:        schema.TypeSet,
 										Required:    true,
-										MaxItems:    1,
-										Description: "Notification group for configuring alert notifications, including recipients' email addresses and alert settings. To ensure either recipient_email_ids or contact_groups is provided",
+										MaxItems:    5,
+										Description: "List of Notification group for configuring alert notifications, including recipients' email addresses and alert settings. To ensure either recipient_email_ids or contact_groups is provided",
 										Elem: &schema.Resource{
 											Schema: map[string]*schema.Schema{
 												"notify_on_warning": {
